1896192b095fe032e58d744570aab5a659d7cfae
Marius Ciubotariu <marius.ciubotariu@gmail.com>
2019-10-13 12:03:31 +0100

    trying to do profiling, comparing to c++

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+extern crate sysinfo;
+use sysinfo::{System, SystemExt, ProcessExt};
+
+use std::process;
+use std::{thread, time};
+
+extern crate chrono;
+use chrono::{Datelike, Timelike, Utc};
+
+use std::fs::File;
+use std::io::Read;
+
+fn main() {
+
+
+    let pid = process::id() as i32;
+    let mut count = 0;
+    let mut config_data = String::new();
+
+    loop {
+
+        let mut sys = System::new();
+        sys.refresh_all();
+        count += 1;
+
+        if count % 5 == 0 {
+            let mut config_file = File::open("src/data.txt").unwrap();
+            let mut new_data = String::new();
+            config_file.read_to_string(&mut new_data).unwrap();
+            // config_data = config_data + &new_data;
+        }
+
+        let now = Utc::now();
+
+        let (is_pm, hour) = now.hour12();
+        let time = format!(
+            "{:02}:{:02}:{:02} {}",
+            hour,
+            now.minute(),
+            now.second(),
+            if is_pm { "PM" } else { "AM" }
+        );
+
+        let pinfo : &sysinfo::Process = sys.get_process(pid).unwrap();
+        println!("[{}] {} pid={} | Name={} | CpuUsage={:.5}% | MemUsage={}MB",
+            time,
+            count,
+            pinfo.pid(),
+            pinfo.name(),
+            pinfo.cpu_usage(),
+            pinfo.memory() / 1024);
+
+        // To refresh all system information:
+        // sys.refresh_all();
+        // sys.reset();
+
+        thread::sleep(time::Duration::from_millis(5000));
+    }
+
+
+
+}
---
+++ usage.cpp
@@ -0,0 +1,189 @@
+
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <thread>
+#include <ctime>
+#include <vector>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+/*
+ * Author:  David Robert Nadeau
+ * Site:    http://NadeauSoftware.com/
+ * License: Creative Commons Attribution 3.0 Unported License
+ *          http://creativecommons.org/licenses/by/3.0/deed.en_US
+ */
+
+#if defined(_WIN32)
+#include <windows.h>
+#include <psapi.h>
+
+#elif defined(__unix__) || defined(__unix) || defined(unix) || (defined(__APPLE__) && defined(__MACH__))
+#include <unistd.h>
+#include <sys/resource.h>
+
+#if defined(__APPLE__) && defined(__MACH__)
+#include <mach/mach.h>
+
+#elif (defined(_AIX) || defined(__TOS__AIX__)) || (defined(__sun__) || defined(__sun) || defined(sun) && (defined(__SVR4) || defined(__svr4__)))
+#include <fcntl.h>
+#include <procfs.h>
+
+#elif defined(__linux__) || defined(__linux) || defined(linux) || defined(__gnu_linux__)
+#include <stdio.h>
+
+#endif
+
+#else
+#error "Cannot define getPeakRSS( ) or getCurrentRSS( ) for an unknown OS."
+#endif
+
+
+
+
+
+/**
+ * Returns the peak (maximum so far) resident set size (physical
+ * memory use) measured in bytes, or zero if the value cannot be
+ * determined on this OS.
+ */
+size_t getPeakRSS( )
+{
+#if defined(_WIN32)
+    /* Windows -------------------------------------------------- */
+    PROCESS_MEMORY_COUNTERS info;
+    GetProcessMemoryInfo( GetCurrentProcess( ), &info, sizeof(info) );
+    return (size_t)info.PeakWorkingSetSize;
+
+#elif (defined(_AIX) || defined(__TOS__AIX__)) || (defined(__sun__) || defined(__sun) || defined(sun) && (defined(__SVR4) || defined(__svr4__)))
+    /* AIX and Solaris ------------------------------------------ */
+    struct psinfo psinfo;
+    int fd = -1;
+    if ( (fd = open( "/proc/self/psinfo", O_RDONLY )) == -1 )
+        return (size_t)0L;      /* Can't open? */
+    if ( read( fd, &psinfo, sizeof(psinfo) ) != sizeof(psinfo) )
+    {
+        close( fd );
+        return (size_t)0L;      /* Can't read? */
+    }
+    close( fd );
+    return (size_t)(psinfo.pr_rssize * 1024L);
+
+#elif defined(__unix__) || defined(__unix) || defined(unix) || (defined(__APPLE__) && defined(__MACH__))
+    /* BSD, Linux, and OSX -------------------------------------- */
+    struct rusage rusage;
+    getrusage( RUSAGE_SELF, &rusage );
+#if defined(__APPLE__) && defined(__MACH__)
+    return (size_t)rusage.ru_maxrss;
+#else
+    return (size_t)(rusage.ru_maxrss * 1024L);
+#endif
+
+#else
+    /* Unknown OS ----------------------------------------------- */
+    return (size_t)0L;          /* Unsupported. */
+#endif
+}
+
+
+
+
+
+/**
+ * Returns the current resident set size (physical memory use) measured
+ * in bytes, or zero if the value cannot be determined on this OS.
+ */
+size_t getCurrentRSS( )
+{
+#if defined(_WIN32)
+    /* Windows -------------------------------------------------- */
+    PROCESS_MEMORY_COUNTERS info;
+    GetProcessMemoryInfo( GetCurrentProcess( ), &info, sizeof(info) );
+    return (size_t)info.WorkingSetSize;
+
+#elif defined(__APPLE__) && defined(__MACH__)
+    /* OSX ------------------------------------------------------ */
+    struct mach_task_basic_info info;
+    mach_msg_type_number_t infoCount = MACH_TASK_BASIC_INFO_COUNT;
+    if ( task_info( mach_task_self( ), MACH_TASK_BASIC_INFO,
+        (task_info_t)&info, &infoCount ) != KERN_SUCCESS )
+        return (size_t)0L;      /* Can't access? */
+    return (size_t)info.resident_size;
+
+#elif defined(__linux__) || defined(__linux) || defined(linux) || defined(__gnu_linux__)
+    /* Linux ---------------------------------------------------- */
+    long rss = 0L;
+    FILE* fp = NULL;
+    if ( (fp = fopen( "/proc/self/statm", "r" )) == NULL )
+        return (size_t)0L;      /* Can't open? */
+    if ( fscanf( fp, "%*s%ld", &rss ) != 1 )
+    {
+        fclose( fp );
+        return (size_t)0L;      /* Can't read? */
+    }
+    fclose( fp );
+    return (size_t)rss * (size_t)sysconf( _SC_PAGESIZE);
+
+#else
+    /* AIX, BSD, Solaris, and Unknown OS ------------------------ */
+    return (size_t)0L;          /* Unsupported. */
+#endif
+}
+
+using namespace std;
+
+auto read_file() -> std::vector<std::string>
+{
+    std::vector<std::string> data;
+    std::string line;
+    ifstream myfile("src/data.txt");
+
+    if (myfile.is_open())
+    {
+        while (getline (myfile, line))
+        {
+            data.push_back(line);
+        }
+        myfile.close();
+    }
+
+    return data;
+}
+
+int main () {
+
+    // auto data;
+    int count = 0;
+    std::vector<std::string> all_data;
+    while (true)
+    {
+        count++;
+        if (count % 5 == 0)
+        {
+            auto data = read_file();
+            all_data.insert(
+                std::end(all_data),
+                std::begin(data),
+                std::end(data));
+        }
+
+        auto now = std::chrono::system_clock::now();
+        std::time_t time_now = std::chrono::system_clock::to_time_t(now);
+        std::string t( ctime( &time_now ) );
+        std::stringstream stime;
+        stime << t.substr( 0, t.length() -1  );
+
+        std::cout
+            << "[" << stime.str() << "] " << count << " pid=" << getpid()
+            << " rss=" << float(getCurrentRSS()) / 1024 / 1024
+            << "MB " << std::endl;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+    }
+
+    return 0;
+}
